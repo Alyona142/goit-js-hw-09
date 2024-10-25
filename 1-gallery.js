67f19848b15a381d66874dc0f55d806cb9b5f91d
Alyona142 <olenapav97@gmail.com>
2024-10-25 12:09:59 +0000

    Deploying to gh-pages from @ Alyona142/goit-js-hw-09@28aedb16d7996c53be7ea9f51f77da8c06882153 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{S as a}from"./assets/vendor-B-WyX8ve.js";document.querySelector(".gallery");galleryContainer.innerHTML=galleryMarkup;new a(".gallery-link",{captions:!0,captionsData:"alt",captionDelay:250});
+//# sourceMappingURL=1-gallery.js.map
